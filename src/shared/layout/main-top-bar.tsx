19ae65933a03b6911d6904a121ebce4c3aa0f64e
lofi <jsj2505@gmail.com>
2025-01-13 09:47:53 +0900

    [fix] tailwind 미적용 fix

--- src/shared/layout/main-top-bar.tsx
+++ src/shared/layout/main-top-bar.tsx
@@ -1,5 +1,18 @@
+import Jera from '../icon/jera.svg';
+
 const MainTopBar = () => {
-  return <header className='sticky top-0 w-full'>Header</header>;
+  return (
+    <header className='sticky top-0 w-full flex justify-between'>
+      <div className='px-4 py-2'>
+        <div className='flex items-center gap-3'>
+          <Jera className='w-7 h-7' />
+          <h1 className='font-bold text-xl'>Jera</h1>
+        </div>
+        <nav></nav>
+      </div>
+      <div>d</div>
+    </header>
+  );
 };
 
 export default MainTopBar;
