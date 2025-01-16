058cfa0744f412a56a327a00435d164e068a6269
lofi <jsj2505@gmail.com>
2025-01-16 13:55:19 +0900

    [config] disable react strict mode

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  reactStrictMode: false,
   webpack(config) {
     // Grab the existing rule that handles SVG imports
     const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.('.svg'));
