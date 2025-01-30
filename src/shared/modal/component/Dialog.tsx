77fd0117c5400f21b53c77783aa6586c11dece1e
lofi <jsj2505@gmail.com>
2025-01-30 18:08:57 +0900

    [Design] modal 기본 템플릿

--- src/shared/modal/component/Dialog.tsx
+++ src/shared/modal/component/Dialog.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from 'react';
 
 const Dialog = ({ children }: PropsWithChildren) => {
   return (
-    <div className='fixed inset-0 flex items-center justify-center bg-opacity-50 backdrop-blur-md z-50'>
+    <div className='fixed inset-0 flex items-center justify-center bg-opacity-50 backdrop-blur-sm z-50'>
       <div className='bg-popover border color p-3 rounded-lg shadow-lg w-[90%] max-w-md animate-fade-in'>
         {children}
       </div>
